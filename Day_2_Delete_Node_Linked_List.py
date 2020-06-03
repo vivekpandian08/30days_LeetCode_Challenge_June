54a38ae78a8cc4d4046359ad3be64e77b319c982
Vivekpandian <vivekpandian08@gmail.com>
2020-06-02 20:55:58 -0500

    day_2_delete_node_linked_list

---
+++ Day_2_Delete_Node_Linked_List.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 20:50:11 2020
+
+@author: vivek
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
