04f659862e45428c204f44ab5d6e1e4dcd66e960
Vivekpandian <vivekpandian08@gmail.com>
2020-06-05 23:51:24 -0500

    Random_pick_with_weight

---
+++ Day_5_Random_pick_with_Weight.py
@@ -0,0 +1,19 @@
+import bisect
+import random
+class Solution(object):
+
+    def __init__(self, w):
+        """
+        :type w: List[int]
+        """
+        self.prefisSum = w
+        for i in range(1, len(self.prefisSum)):
+            self.prefisSum[i] = self.prefisSum[i] + self.prefisSum[i - 1]
+
+
+    def pickIndex(self):
+        """
+        :rtype: int
+        """
+        target = random.randint(1, self.prefisSum[-1])
+        return bisect.bisect_left(self.prefisSum, target)
